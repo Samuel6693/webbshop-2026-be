1b83850b5ba491b20dcc7e2228d23bee543bb98a
Samuel Soumi <samuel_somy93@hotmail.com>
2026-04-18 13:11:49 +0200

    Add function to send SSE events to connected clients

--- src/routes/products.js
+++ src/routes/products.js
@@ -13,6 +13,13 @@ const productsRouter = Router();
 
 const clients = []; // Array to hold connected clients for SSE
 
+// function to send events for alla clients 
+function sendProductEvent(payload) {
+  clients.forEach((client) => {
+    client.write(`data: ${JSON.stringify(payload)}\n\n`);
+  });
+}
+
 productsRouter.get("/", async (req, res) => {
   try {
     const products = await getAllProducts();
